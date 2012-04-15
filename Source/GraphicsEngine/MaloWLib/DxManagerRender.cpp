ee1ccc13d5bac5b42cfd03c36a3ead885485d978
Tillman <reetopps6@hotmail.com>
2012-04-15 18:22:29 +0200

    Now sets render target & depth stencil and view port in RenderAntiAliasing()
    Signed-off-by: Tillman <reetopps6@hotmail.com>

--- Source/GraphicsEngine/MaloWLib/DxManagerRender.cpp
+++ Source/GraphicsEngine/MaloWLib/DxManagerRender.cpp
@@ -571,6 +571,10 @@ void DxManager::RenderAntiAliasing()
 {
 	if(!this->fxaa) return;
 
+	//set render target & depth stencil and viewport
+	this->Dx_DeviceContext->OMSetRenderTargets(1, &this->Dx_RenderTargetView, this->Dx_DepthStencilView);
+	this->Dx_DeviceContext->RSSetViewports(1, &this->Dx_Viewport);
+
 	//set input assembler params
 	this->Dx_DeviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP);
 
