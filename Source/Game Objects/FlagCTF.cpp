03b044ec33da63bac16bf9e99f249de4dec8a5fe
Tillman <reetopps6@hotmail.com>
2012-05-25 14:41:01 +0200

    Added outcommented code to be used for next push
    Signed-off-by: Tillman <reetopps6@hotmail.com>

--- Source/Game Objects/FlagCTF.cpp
+++ Source/Game Objects/FlagCTF.cpp
@@ -26,3 +26,34 @@ void FlagCTF::Reset()
 	this->mMesh->SetPosition(this->mStartPosition);
 	this->mAtBase = true;
 }
+/*
+#include "FlagCTF.h"
+
+FlagCTF::FlagCTF()
+{
+	this->mAtBase = true;
+	this->mStartPosition = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+	this->mAnimatedMesh = NULL;
+}
+
+FlagCTF::FlagCTF(AnimatedMesh* mesh, D3DXVECTOR3 startPosition)
+{
+	this->mAtBase = true;
+	this->mStartPosition = startPosition;
+	this->mAnimatedMesh = mesh;
+}
+FlagCTF::~FlagCTF()
+{
+	
+}
+
+void FlagCTF::SetToStartPosition()
+{
+	this->mAnimatedMesh->SetPosition(this->mStartPosition);
+}
+
+void FlagCTF::Reset()
+{
+	this->mAnimatedMesh->SetPosition(this->mStartPosition);
+	this->mAtBase = true;
+}*/
